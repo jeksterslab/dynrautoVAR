6e8dd64f02c4e520236b17bcdc96344889e0e004
Ivan Jacob Agaloos Pesigan <jeksterslab@users.noreply.github.com>
2023-06-22 09:06:26 +0000

    Automated build.

--- tools/dotfiles.sh
+++ tools/dotfiles.sh
@@ -6,10 +6,10 @@ set -e
 git clone https://github.com/jeksterslab/dotfiles.git
 cd dotfiles
 (cd .setup/bash && make) || echo Building .bashrc failed.
-# (cd .setup/git && make) || echo Building .gitconfig failed.
-# (cd .setup/ssh && make) || echo Building ssh keys failed.
+(cd .setup/git && make) || echo Building .gitconfig failed.
+(cd .setup/ssh && make) || echo Building ssh keys failed.
 (cd .setup/tmux && make) || echo Building .tmux.conf failed.
 (cd .setup/vim && make) || echo Building .vimrc failed.
-# (cd .setup/xterm && make) || echo Building .Xresources failed.
+(cd .setup/xterm && make) || echo Building .Xresources failed.
 cd ..
 rm -rf dotfiles
